00326ce3fe5783a50e13d3f32c2dff594ab516d8
JOSE EDUARDO OSPINA HIGUITA <ospinahiguita@gmail.com>
2023-09-28 22:39:13 -0500

    Creación de entidades

---
+++ Gea/Gea.shared/Entities/Eventoacademico.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gea.shared.Entities
+{
+    public class Eventoacademico
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null!;
+
+        public DateTime FechaInicio { get; set; }
+
+        public DateTime FechaFin { get; set; }
+
+        [Display(Name = "Ubicación")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string Ubicacion { get; set; } = null!;
+
+        [Display(Name = "Descripción")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string Descripcion { get; set; } = null!;
+
+        [Display(Name = "Tema Evento")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string Temaevento { get; set; } = null!;
+    }
+
+    public class Participante
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null!;
+
+        [Display(Name = "Afiliación Institucional")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string Afinstitucional { get; set; } = null!;
+
+        [Display(Name = "Área de interés")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string Area { get; set; } = null!;
+
+        [Display(Name = "Tipo de participación")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string Tipoparticipacion { get; set; } = null!;
+    }
+
+    public class Programaevento
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null!;
+
+        [Display(Name = "Horario")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string Horario { get; set; } = null!;
+
+        [Display(Name = "Sesiones")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string Sesiones { get; set; } = null!;
+
+        [Display(Name = "Ponentes")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string Ponentes { get; set; } = null!;
+
+        [Display(Name = "Temas")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string Temas { get; set; } = null!;
+    }
+}
